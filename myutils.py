aa4f7dc53146a21a9926091beab6a9cf7ba72d44
Nickchen Nick <nickchen120235@gmail.com>
2020-11-14 21:47:15 +0800

    code cleanup

--- myutils.py
+++ myutils.py
@@ -11,22 +11,27 @@ def ipToBytearray(ip: str) -> bytearray:
 
 def ip_checksum(header: bytearray, source: bytearray, dest: bytearray) -> bytearray:
   arr = [n for n in (header+source+dest)]
-  print('header: {}'.format(header))
-  print('source: {}'.format(source))
-  print('dest: {}'.format(dest))
-  print('arr: {}'.format(arr))
   sum = 0
 
   for i in range(0, len(arr), 2):
-    print('*****************************')
     sum += (arr[i] << 8) + arr[i+1]
-    print('[{}]: {}'.format(i, arr[i]))
-    print('[{}]: {}'.format(i+1, arr[i+1]))
-    print('sum = {}'.format(hex(sum)))
   
   carry = int(hex(sum)[:-4], 16)
   check = 0xffff - (int(hex(sum)[-4:], 16) + carry)
   return bytearray.fromhex(hex(check)[-4:])
+
+def ipv4_packet(protocol: int, ttl: int, src: str, dest: str, nextHeader: bytearray) -> bytearray:
+  ver_IHL_TOS = b'\x45\x00'
+  length = (20+len(nextHeader)).to_bytes(2, 'big')
+  ID = b'\xde\xad'
+  flags = b'\x00\x00'
+  TTL = ttl.to_bytes(1, 'big')
+  proto = protocol.to_bytes(1, 'big')
+  srcIP = ipToBytearray(src)
+  destIP = ipToBytearray(dest)
+  checksum = ip_checksum(ver_IHL_TOS+length+ID+flags+TTL+proto, srcIP, destIP)
+  packet = ver_IHL_TOS+length+ID+flags+TTL+proto+checksum+srcIP+destIP
+  return packet
 
 def tcp_checksum(header: bytearray, source: bytearray, dest: bytearray) -> bytearray:
   arr = [n for n in (header+source+dest)]
@@ -36,11 +41,7 @@ def tcp_checksum(header: bytearray, source: bytearray, dest: bytearray) -> bytea
 
   sum += 26
   for i in range(0, len(arr), 2):
-    print('*****************************')
     sum += (arr[i] << 8) + arr[i+1]
-    print('[{}]: {}'.format(i, arr[i]))
-    print('[{}]: {}'.format(i+1, arr[i+1]))
-    print('sum = {}'.format(hex(sum)))
 
   carry = int(hex(sum)[:-4], 16)
   check = 0xffff - (int(hex(sum)[-4:], 16) + carry)
@@ -48,23 +49,14 @@ def tcp_checksum(header: bytearray, source: bytearray, dest: bytearray) -> bytea
 
 def tcp_packet(source_port: int, dest_port: int, source_ip: str, dest_ip: str, seq_num: int, ack_num: int, ack: bool=False, rst: bool=False, syn: bool=False, fin: bool=False) -> bytearray:
   source = bytearray.fromhex('{0:#0{1}x}'.format(source_port, 6)[2:])
-  print(f'source: {source}')
   dest = bytearray.fromhex('{0:#0{1}x}'.format(dest_port, 6)[2:])
-  print(f'dest: {dest}')
   seq_arr = bytearray.fromhex('{0:#0{1}x}'.format(seq_num, 10)[2:])
-  print(f'seq_arr: {seq_arr}')
   ack_arr = bytearray.fromhex('{0:#0{1}x}'.format(ack_num, 10)[2:])
-  print(f'ack_arr: {ack_arr}')
   flag = '0b01010000000' + ('1' if ack else '0') + '0' + ('1' if rst else '0') + ('1' if syn else '0') + ('1' if fin else '0')
-  print(f'flag: {flag} (ack: {ack}, rst: {rst}, syn: {syn}, fin: {fin})')
   flag_arr = bytearray.fromhex(hex(int(flag, 2))[2:])
-  print(f'flag_arr: {flag_arr}')
   window = b'\x71\x10'
-  print(f'window: {window}')
   checksum = tcp_checksum(source+dest+seq_arr+ack_arr+flag_arr+window, ipToBytearray(source_ip), ipToBytearray(dest_ip))
-  print(f'checksum: {checksum}')
   packet = source+dest+seq_arr+ack_arr+flag_arr+window+checksum+b'\x00\x00'
-  print(f'packet: {packet}, length: {len(packet)}')
   return packet
 
 def macToBytearray(mac: str) -> bytearray:
@@ -73,61 +65,56 @@ def macToBytearray(mac: str) -> bytearray:
 def ether_packet(dest_mac: str,src_mac: str) -> bytearray:
   return macToBytearray(dest_mac)+macToBytearray(src_mac)+b'\x08\x00'
 
+def udp_checksum(header: bytearray, source: bytearray, dest: bytearray) -> bytearray:
+  arr = [n for n in (header+source+dest)]
+  sum = 0
+
+  sum += 8 # UDP Length
+  sum += 17 # IPPROTO_UDP
+  for i in range(0, len(arr), 2):
+    sum += (arr[i] << 8) + arr[i+1]
+
+  carry = int(hex(sum)[:-4], 16)
+  check = 0xffff - (int(hex(sum)[-4:], 16) + carry)
+  return bytearray.fromhex(hex(check)[-4:])
+
+def udp_packet(source_port: int, dest_port: int, source_ip: str, dest_ip: str) -> bytearray:
+  source = bytearray.fromhex('{0:#0{1}x}'.format(source_port, 6)[2:])
+  dest = bytearray.fromhex('{0:#0{1}x}'.format(dest_port, 6)[2:])
+  length = b'\x00\x08'
+  checksum = udp_checksum(source+dest+length, ipToBytearray(source_ip), ipToBytearray(dest_ip))
+  packet = source + dest + length + checksum
+  return packet
+
 def unpackage(arr: bytearray) -> dict:
   ether = arr[0:14]
-  print(f'ether: {ether}')
   destMac = ether[0:6].hex(':')
-  print(f'destMac: {destMac}')
-  srcMac = ether[6:12].hex(':')
-  print(f'srcMac: {srcMac}\n')
-  
+  srcMac = ether[6:12].hex(':')  
 
   ip = arr[14:34]
-  print(f'ip: {ip}')
   IPver = int(ip[0]) >> 4
-  print(f'IPver: {IPver}')
   IHL = int(ip[0]) - (IPver << 4)
-  print(f'IHL: {IHL}')
   length = (int(ip[2]) << 8) + int(ip[3])
-  print(f'Length: {length}')
   ID = hex((int(ip[4]) << 8) + int(ip[5]))
-  print(f'ID: {ID}')
   ttl = int(ip[8])
-  print(f'TTL: {ttl}')
   proto = int(ip[9])
-  print(f'protocol: {proto}')
   IPchecksum = hex((int(ip[10]) << 8) + int(ip[11]))
-  print(f'IPchecksum: {IPchecksum}')
   srcIP = f'{int(ip[12])}.{int(ip[13])}.{int(ip[14])}.{int(ip[15])}'
-  print(f'srcIP: {srcIP}')
   destIP = f'{int(ip[16])}.{int(ip[17])}.{int(ip[18])}.{int(ip[19])}'
-  print(f'destIP: {destIP}\n')
 
   tcp = arr[34:54]
-  print(f'tcp: {tcp}')
   srcPort = (int(tcp[0]) << 8) + int(tcp[1])
-  print(f'srcPort: {srcPort}')
   destPort = (int(tcp[2]) << 8) + int(tcp[3])
-  print(f'destPort: {destPort}')
   seq_num = (int(tcp[4]) << 24) + (int(tcp[5]) << 16) + (int(tcp[6]) << 8) + int(tcp[7])
-  print(f'seq_num: {seq_num}')
   ack_num = (int(tcp[8]) << 24) + (int(tcp[9]) << 16) + (int(tcp[10]) << 8) + int(tcp[11])
-  print(f'ack_num: {ack_num}')
   flags = bin((int(tcp[12]) << 8) + int(tcp[13]))[2:].zfill(16)
   offset = int(flags[0:4], 2)
-  print(f'offset: {offset} ({flags[0:4]})')
   ack = True if flags[11] == '1' else False
-  print(f'ack: {ack}')
   rst = True if flags[13] == '1' else False
-  print(f'rst: {rst}')
   syn = True if flags[14] == '1' else False
-  print(f'syn: {syn}')
   fin = True if flags[15] == '1' else False
-  print(f'fin: {fin}')
   window = (int(tcp[14]) << 8) + int(tcp[15])
-  print(f'window: {window}')
   TCPchecksum = hex((int(tcp[16]) << 8) + int(tcp[17]))
-  print(f'TCPchecksum: {TCPchecksum}')
 
   return {
     'ether': {
@@ -159,47 +146,4 @@ def unpackage(arr: bytearray) -> dict:
       'checksum': TCPchecksum
     }
   }
-def udp_checksum(header: bytearray, source: bytearray, dest: bytearray) -> bytearray:
-  arr = [n for n in (header+source+dest)]
-  print('header: {}'.format(header))
-  print('source: {}'.format(source))
-  print('dest: {}'.format(dest))
-  print('arr: {}'.format(arr))
-  sum = 0
 
-  sum += 8 # UDP Length
-  sum += 17 # IPPROTO_UDP
-  for i in range(0, len(arr), 2):
-    print('*****************************')
-    sum += (arr[i] << 8) + arr[i+1]
-    print('[{}]: {}'.format(i, arr[i]))
-    print('[{}]: {}'.format(i+1, arr[i+1]))
-    print('sum = {}'.format(hex(sum)))
-  carry = int(hex(sum)[:-4], 16)
-  check = 0xffff - (int(hex(sum)[-4:], 16) + carry)
-  return bytearray.fromhex(hex(check)[-4:])
-
-def udp_packet(source_port: int, dest_port: int, source_ip: str, dest_ip: str) -> bytearray:
-  source = bytearray.fromhex('{0:#0{1}x}'.format(source_port, 6)[2:])
-  print(f'source: {source}')
-  dest = bytearray.fromhex('{0:#0{1}x}'.format(dest_port, 6)[2:])
-  print(f'dest: {dest}')
-  length = b'\x00\x08'
-  checksum = udp_checksum(source+dest+length, ipToBytearray(source_ip), ipToBytearray(dest_ip))
-  print(f'checksum: {checksum}')
-  packet = source + dest + length + checksum
-  print(f'packet: {packet}, length: {len(packet)}')
-  return packet
-
-def ipv4_packet(protocol: int, ttl: int, src: str, dest: str, nextHeader: bytearray) -> bytearray:
-  ver_IHL_TOS = b'\x45\x00'
-  length = (20+len(nextHeader)).to_bytes(2, 'big')
-  ID = b'\xde\xad'
-  flags = b'\x00\x00'
-  TTL = ttl.to_bytes(1, 'big')
-  proto = protocol.to_bytes(1, 'big')
-  srcIP = ipToBytearray(src)
-  destIP = ipToBytearray(dest)
-  checksum = ip_checksum(ver_IHL_TOS+length+ID+flags+TTL+proto, srcIP, destIP)
-  packet = ver_IHL_TOS+length+ID+flags+TTL+proto+checksum+srcIP+destIP
-  return packet
