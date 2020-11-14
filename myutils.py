439892e5f8a35658fca7d5f1f0d8b6c50c0e87f3
Nickchen Nick <nickchen120235@gmail.com>
2020-11-14 20:42:42 +0800

    add unpackage function

--- myutils.py
+++ myutils.py
@@ -88,3 +88,90 @@ def macToBytearray(mac: str) -> bytearray:
 
 def ether_packet(dest_mac: str,src_mac: str) -> bytearray:
   return macToBytearray(dest_mac)+macToBytearray(src_mac)+b'\x08\x00'
+
+def unpackage(arr: bytearray) -> dict:
+  ether = arr[0:14]
+  print(f'ether: {ether}')
+  destMac = ether[0:6].hex(':')
+  print(f'destMac: {destMac}')
+  srcMac = ether[6:12].hex(':')
+  print(f'srcMac: {srcMac}\n')
+  
+
+  ip = arr[14:34]
+  print(f'ip: {ip}')
+  IPver = int(ip[0]) >> 4
+  print(f'IPver: {IPver}')
+  IHL = int(ip[0]) - (IPver << 4)
+  print(f'IHL: {IHL}')
+  length = (int(ip[2]) << 8) + int(ip[3])
+  print(f'Length: {length}')
+  ID = hex((int(ip[4]) << 8) + int(ip[5]))
+  print(f'ID: {ID}')
+  ttl = int(ip[8])
+  print(f'TTL: {ttl}')
+  proto = int(ip[9])
+  print(f'protocol: {proto}')
+  IPchecksum = hex((int(ip[10]) << 8) + int(ip[11]))
+  print(f'IPchecksum: {IPchecksum}')
+  srcIP = f'{int(ip[12])}.{int(ip[13])}.{int(ip[14])}.{int(ip[15])}'
+  print(f'srcIP: {srcIP}')
+  destIP = f'{int(ip[16])}.{int(ip[17])}.{int(ip[18])}.{int(ip[19])}'
+  print(f'destIP: {destIP}\n')
+
+  tcp = arr[34:54]
+  print(f'tcp: {tcp}')
+  srcPort = (int(tcp[0]) << 8) + int(tcp[1])
+  print(f'srcPort: {srcPort}')
+  destPort = (int(tcp[2]) << 8) + int(tcp[3])
+  print(f'destPort: {destPort}')
+  seq_num = (int(tcp[4]) << 24) + (int(tcp[5]) << 16) + (int(tcp[6]) << 8) + int(tcp[7])
+  print(f'seq_num: {seq_num}')
+  ack_num = (int(tcp[8]) << 24) + (int(tcp[9]) << 16) + (int(tcp[10]) << 8) + int(tcp[11])
+  print(f'ack_num: {ack_num}')
+  flags = bin((int(tcp[12]) << 8) + int(tcp[13]))[2:].zfill(16)
+  offset = int(flags[0:4], 2)
+  print(f'offset: {offset} ({flags[0:4]})')
+  ack = True if flags[11] == '1' else False
+  print(f'ack: {ack}')
+  rst = True if flags[13] == '1' else False
+  print(f'rst: {rst}')
+  syn = True if flags[14] == '1' else False
+  print(f'syn: {syn}')
+  fin = True if flags[15] == '1' else False
+  print(f'fin: {fin}')
+  window = (int(tcp[14]) << 8) + int(tcp[15])
+  print(f'window: {window}')
+  TCPchecksum = hex((int(tcp[16]) << 8) + int(tcp[17]))
+  print(f'TCPchecksum: {TCPchecksum}')
+
+  return {
+    'ether': {
+      'dest': destMac,
+      'src': srcMac
+    },
+    'IP': {
+      'ver': IPver,
+      'IHL': IHL,
+      'length': length,
+      'ID': ID,
+      'ttl': ttl,
+      'proto': proto,
+      'checksum': IPchecksum,
+      'src': srcIP,
+      'dest': destIP
+    },
+    'TCP': {
+      'src': srcPort,
+      'dest': destPort,
+      'seq_num': seq_num,
+      'ack_num': ack_num,
+      'offset': offset,
+      'ack': ack,
+      'rst': rst,
+      'syn': syn,
+      'fin': fin,
+      'window': window,
+      'checksum': TCPchecksum
+    }
+  }
