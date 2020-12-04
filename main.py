f63992b2b56eda0ef8b2c4824067ae5f4df358c2
Nickchen Nick <nickchen120235@gmail.com>
2020-12-05 00:11:26 +0800

    ICMP WIP

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from PySide2.QtWidgets import *
 
 from views.ARP import ARPView
 from views.UDP import UDPView
+from views.ICMP import ICMPView
 
 class MainView(QTabWidget):
   def __init__(self, parent = None):
     super(MainView, self).__init__(parent)
     self.tab1 = ARPView()
     self.tab3 = UDPView()
+    self.tab4 = ICMPView()
 
     self.addTab(self.tab1, 'ARP')
     self.addTab(self.tab3, 'UDP')
+    self.addTab(self.tab4, 'ICMP')
 
     self.setWindowTitle('Packet Sender')
 
---
+++ views/ICMP.py
@@ -0,0 +1,95 @@
+""" 
+View: ICMP
+Widgets:
+  Setup
+  Result
+"""
+
+from PySide2.QtWidgets import *
+
+from views.widgets.info.ether import EtherInfo
+from views.widgets.info.ipv4 import IPv4Info
+from views.widgets.info.icmp import ICMPInfo
+
+_ops = ['Echo Reply (0)', 'Echo Request (8)']
+
+class ICMPView(QWidget):
+  def __init__(self):
+    super(ICMPView, self).__init__()
+    layout = QVBoxLayout()
+    self.setLayout(layout)
+
+    """
+    Subview: Setup
+    Widgets: 
+      iface: QLineEdit + QPushButton
+      localIP: QLabel
+      tarIP: QLineEdit
+      op: QComboBox
+      code: QLineEdit
+      sendBtn: QPushButton
+    """
+    setup = QGroupBox('Setup')
+    layout.addWidget(setup)
+    setupContent = QFormLayout()
+    setup.setLayout(setupContent)
+
+    iface = QHBoxLayout()
+    ifaceInput = QLineEdit()
+    iface.addWidget(ifaceInput)
+    ifaceSet = QPushButton('Set')
+    iface.addWidget(ifaceSet)
+    setupContent.addRow('Interface: ', iface)
+
+    localIP = QLabel('Unknown')
+    setupContent.addRow('Local IP: ', localIP)
+
+    tarIP = QLineEdit()
+    setupContent.addRow('Target IP: ', tarIP)
+
+    op = QComboBox()
+    op.addItems(_ops)
+    setupContent.addRow('Type: ', op)
+
+    code = QLineEdit()
+    setupContent.addRow('Code: ', code)
+
+    sendBtn = QPushButton('Send')
+    setupContent.addWidget(sendBtn)
+
+    """Subview: Request
+    Widgets:
+      ether: QGroupBox
+      ip: QGroupBox
+      icmp: QGroupBox
+    """
+    req = QGroupBox('Request')
+    layout.addWidget(req)
+    reqContent = QVBoxLayout()
+    reqContent.addStretch(5)
+    req.setLayout(reqContent)
+
+    req_ether = EtherInfo()
+    reqContent.addWidget(req_ether)
+    req_ipv4 = IPv4Info()
+    reqContent.addWidget(req_ipv4)
+    req_icmp = ICMPInfo()
+    reqContent.addWidget(req_icmp)
+
+    """Subview: Result
+    Widgets:
+      ether: QGroupBox
+      ip: QGroupBox
+      icmp: QGroupBox
+    """
+    res = QGroupBox('Result')
+    layout.addWidget(res)
+    resContent = QVBoxLayout()
+    res.setLayout(resContent)
+
+    res_ether = EtherInfo()
+    resContent.addWidget(res_ether)
+    res_ipv4 = IPv4Info()
+    resContent.addWidget(res_ipv4)
+    res_icmp = ICMPInfo()
+    resContent.addWidget(res_icmp)
